d4e1a325ebbdc8bf026303e94045ffd3124915b8
Glorinha <liviafariabraz282@gmail.com>
2024-04-19 23:07:20 -0300

    :sparkles: feat: pagina de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    if (request.method == "GET"):
+        return render(request, 'cadastro.html')
+    elif (request.method == "POST"):
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        users = User.objects.filter(username=username)
+        print(users.exists())
+
+        if (users.exists()):
+            messages.add_message(request, constants.ERROR, "Já existe um usuário com esse username")
+            return redirect('/usuarios/cadastro/')  
+        
+        if (senha != confirmar_senha):
+            messages.add_message(request, constants.ERROR, "A senha e o confirmar senha devem ser iguais")
+            return redirect('/usuarios/cadastro')
+        
+        if (len(senha) < 6):
+            messages.add_message(request, constants.ERROR, "A senha de ter mais de 6 dígitos")
+            return redirect('/usuarios/cadastro')
+        
+        user = User.objects.create_user(
+            username = username,
+            email = email,
+            password = senha
+        )
+
+        return redirect('/usuarios/login/')
